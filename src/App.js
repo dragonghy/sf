8cc95c4f2b6bc882792b2ffe983dc3cb41b6e4d6
Huayang Guo <huayang@pinterest.com>
2019-01-03 22:16:07 -0800

    add create modal

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
-
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import './App.css';
 
@@ -31,6 +36,10 @@ const GET_EVENTS = gql`
 `;
 
 class App extends Component {
+  state = {
+    open: false
+  };
+
   render() {
     return (
         <ApolloProvider client={client}>
@@ -43,44 +52,84 @@ class App extends Component {
               </Typography>
             </AppBar>
             <main>
-              <br />
-              <br />
-              <br />
-              <br />
-              <Button variant="contained" color="primary">
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <Button variant="contained" color="primary" onClick={() => this.setState({ open: true})}>
                 New event
               </Button>
-
-              <br />
-              <br />
-              <Paper>
-                <Table>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Title</TableCell>
-                      <TableCell align="right">Start</TableCell>
-                      <TableCell align="right">End</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    <Query query={GET_EVENTS}>
-                      {({ loading, error, data }) => {
-                        if (loading) return "Loading...";
-                        if (error) return `Error! ${error.message}`;
-
-                        const rows = data.events.map(event => (
-                            <TableRow key={event.id}>
-                              <TableCell>{event.title}</TableCell>
-                              <TableCell>{event.startTime ? new Date(event.startTime).toDateString() : ''}</TableCell>
-                              <TableCell>{event.endTime ? new Date(event.endTime).toDateString() : ''}</TableCell>
-                            </TableRow>
-                        ));
-
-                        return rows;
+              <Dialog open={this.state.open} onClose={() => this.setState({ open: false})} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">New event</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>
+                    Create a new event
+                  </DialogContentText>
+                  <TextField
+                      autoFocus
+                      margin="dense"
+                      id="title"
+                      label="Title"
+                  />
+                  <TextField
+                      id="startTime"
+                      label="Start"
+                      type="datetime-local"
+                      InputLabelProps={{
+                        shrink: true,
                       }}
-                    </Query>
-                  </TableBody>
-                </Table>
+                  />
+                  <TextField
+                      id="endTime"
+                      label="End"
+                      type="datetime-local"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={() => this.setState({ open: false})} color="primary">
+                    Cancel
+                  </Button>
+                  <Button onClick={() => this.setState({ open: false})} color="primary">
+                    Create
+                  </Button>
+                </DialogActions>
+              </Dialog>
+
+              <br/>
+              <br/>
+              <Paper>
+                <Query query={GET_EVENTS}>
+                  {({loading, error, data}) => {
+                    if (loading) return "Loading...";
+                    if (error) return `Error! ${error.message}`;
+
+                    const rows = data.events.map(event => (
+                        <TableRow key={event.id}>
+                          <TableCell>{event.title}</TableCell>
+                          <TableCell>{event.startTime ? new Date(event.startTime).toDateString() : ''}</TableCell>
+                          <TableCell>{event.endTime ? new Date(event.endTime).toDateString() : ''}</TableCell>
+                        </TableRow>
+                    ));
+
+                    return (
+                        <Table>
+                          <TableHead>
+                            <TableRow>
+                              <TableCell>Title</TableCell>
+                              <TableCell align="right">Start</TableCell>
+                              <TableCell align="right">End</TableCell>
+                            </TableRow>
+                          </TableHead>
+                          <TableBody>
+                            {rows}
+                          </TableBody>
+                        </Table>
+                    );
+                  }}
+                </Query>
               </Paper>
             </main>
           </div>
